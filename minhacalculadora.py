22abd474571f76d91f2c99869a4b62b579c5a0f6
manafofinha <socorrodejesusmoraesalmeida@gmail.com>
2024-07-10 15:40:41 -0300

    arquivo teste minhacalculadora.py

---
+++ minhacalculadora.py
@@ -0,0 +1,27 @@
+# Criar a classe
+# chamada 'calculadora'
+class Calculadora:
+    
+    #Criar o método
+    # chamado 'soma'
+    def soma(self, n1, n2):
+      return n2 + n1
+ 
+    #Criar o método
+    # chamado 'subtração'
+    def sub(self, n1, n2):
+      return n1 - n2
+     
+    #Criar o método
+    # chamado 'multiplicação'
+    def mult(self, n1, n2):
+      return n1 * n2
+     
+    #Criar o método
+    # chamado 'divisão'
+    def divi(self, n1, n2):
+      return n1 / n2
+  
+ 
+ 
+ 
